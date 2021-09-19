6ae3a641ccef04bd56dae124fdb3f6742a932084
eric robert <eric.robert@simpleclinic.net>
2021-09-19 12:52:48 +1000

    Refactors tests to use lazy static to keep temp file

--- tests/snippet_test.rs
+++ tests/snippet_test.rs
@@ -1,12 +1,18 @@
-use tempfile::tempdir;
+use tempfile::{tempdir, TempDir};
 use std::fs::File;
 use std::io::{ Write};
 use std::vec::Vec;
 use std::path::Path;
 use std::env;
 
+#[macro_use]
+extern crate lazy_static;
+
 use snippetlibrary::{SnippetCollection, Snippet};
 
+lazy_static!{
+    static ref DIR:TempDir = tempdir().unwrap();
+}
 
 #[test]
 fn it_can_init_snippet(){
@@ -37,21 +43,27 @@ fn it_can_get_extension(){
 
 #[test]
 fn it_can_get_content(){
-    let path = env::current_dir().unwrap().join("tests/a-test-file.php");
+    let file_path: String = create_test_file();
 
-    let snippet = Snippet::new(path.into_os_string().into_string().unwrap() );
+    //let path = env::current_dir().unwrap().join("tests/a-test-file.php");
 
-    assert_eq!("<?php echo 'hello';?>", snippet.content());
+    //let snippet = Snippet::new(path.into_os_string().into_string().unwrap() );
+    let snippet = Snippet::new(file_path.to_string() );
+
+    assert_eq!("<?php echo 'hello';?>\n", snippet.content());
 }
 
 fn create_test_file()->String{
-    let dir = tempdir().unwrap();
-    let path=  dir.path().to_str().unwrap().to_string();
+    //let dir = tempdir().unwrap();
+    let path=  DIR.path().to_str().unwrap().to_string();
+    //let path=  dir.path().to_str().unwrap().to_string();
 
 
-    let file_path = dir.path().join("a-php-file.php");
+    let file_path = DIR.path().join("a-php-file.php");
+    //let file_path = dir.path().join("a-php-file.php");
     let mut file = File::create(file_path).expect("Problem");
     writeln!(file, "<?php echo 'hello';?>").expect("Could not write");
 
-    dir.path().join("a-php-file.php").into_os_string().into_string().unwrap()
+    DIR.path().join("a-php-file.php").into_os_string().into_string().unwrap()
+    //dir.path().join("a-php-file.php").into_os_string().into_string().unwrap()
 }
